5d477515a2600a99ca4c44c1fe0a8b224043e165
Lynsey Vandenberg <lynsey.vandenberg@gmail.com>
2021-04-26 00:01:07 -0400

    added functions to game.js

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,22 @@
-window.alert("This is an alert! Javascript is running!");
+// adds player input
+
+var playerName = window.prompt("what is your robot's name?");
+//What is this?
+console.log(playerName);
+console.log("This logs a string, good for leavivng yourself a message");
+// this will do math and log 20
+console.log(10+10);
+// what is this?
+console.log("Our robot's name is " + playerName);
+
+
+
+// Note the lack of quotation marks around the playerName 
+// replacing - window.alert(playerName); - with console.log(playerName);
+// this creates a function named "fight"
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// fight();
